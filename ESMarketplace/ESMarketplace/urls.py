dd522223fa456d787a648f60212fee14611c9ef8
Jerry W <jerrywarner.techgeek@gmail.com>
2019-09-25 03:28:26 -0700

    tutorials stuff

--- ESMarketplace/ESMarketplace/urls.py
+++ ESMarketplace/ESMarketplace/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('main/', include('marketplaceApp.urls')),
+    path('polls/', include('marketplaceApp.urls')),
     path('admin/', admin.site.urls)
 ]
